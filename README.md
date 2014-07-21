8b2906d2d3e531209fdb911809722b4b71e10192
David Dias <daviddias.p@gmail.com>
2014-07-20 20:58:58 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,11 @@
+person-parser
+=======
+
+
+Parses the juice data produced by `person`
+
+example:
+
+```
+cat emails.txt | person | person-parser > juicy.txt;
+```
--- person-parser.js
+++ person-parser.js
@@ -8,20 +8,42 @@ stdin.setEncoding('utf8');
 process.stdout.write('Name\tEmail\tOccupation\tCompany\tTwitter\tLinkedin\tAngel List\n');
 
 stdin
-  .pipe(es.split())
+  // .pipe(es.split())
   .pipe(es.parse())
   .pipe(es.map(function (data, callback) {
     var name = data.rapportive.first_name + ' ' + data.rapportive.last_name;
     var email = data.email;
 
-    // check for more than one, separate by semicolon
-    var occupation = "";
-    var company = "";
-    
-    var twitter = "";   //check for more than 1
-    var linkedin = ""; 
-    var angellist = ""; 
+    var occupation = '';
+    var company = '';
 
+    // if (data.rapportive.occupations.length > 1) {
+    for(var i=0;i<data.rapportive.occupations.length;i++) {
+      occupation = occupation + data.rapportive.occupations[i].job_title + ';';
+      company = company + data.rapportive.occupations[i].company + ';';
+    }
+    // } else {
+    //   occupation = data.rapportive.occupations.job_title;
+    //   company = data.rapportive.occupations.company;
+    // }
+
+    var twitter = ''; 
+    var linkedin = ''; 
+    var angellist = ''; 
+
+    if (data.rapportive.memberships.length > 0) {
+      for(var i=0;i<data.rapportive.memberships.length;i++) { 
+        if (data.rapportive.memberships[i].icon_name === 'linkedin'){
+          linkedin = linkedin + data.rapportive.memberships[i].profile_url + ';';
+        }
+        if (data.rapportive.memberships[i].icon_name === 'twitter'){
+          twitter = twitter + data.rapportive.memberships[i].profile_url + ';';
+        }
+        if (data.rapportive.memberships[i].icon_name === 'angellist'){
+          angellist = angellist + data.rapportive.memberships[i].profile_url + ';'; 
+        }
+      }
+    }
     callback(null, util.format('%s\t%s\t%s\t%s\t%s\t%s\t%s\n', name, email, occupation, company, twitter, linkedin, angellist));
   }))
   .pipe(process.stdout);
